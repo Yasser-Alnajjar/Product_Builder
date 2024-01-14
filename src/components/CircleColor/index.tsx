917b3abd04669d7c6be90a76f8f423d3fdf606fd
Yasser Al-Najjar <yasseralnajjar@outlook.sa>
2024-01-14 23:51:39 +0200

    fix circle color design

--- src/components/CircleColor/index.tsx
+++ src/components/CircleColor/index.tsx
@@ -11,13 +11,13 @@ export default function CircleColor({ color, className, ...rest }: Props) {
     color === "#fff" || color === "#ffffff" ? "#000" : "#ffffff";
   return (
     <Button
-      outline="outline-[color]"
       {...rest}
-      className={`w-5 px-0 py-0 ${className} grid place-content-center  border`}
+      className={`${className} px-0 py-0`}
       style={{
         width: "1.25rem",
         height: "1.25rem",
         borderRadius: "50%",
+        padding: 0,
         backgroundColor: color,
         color: colorIsWhite,
         borderColor: color === "#fff" || color === "#ffffff" ? "#000" : color,
